39e1b778253b14c793969b3c38d573926baae740
Leptonnw <kac4484@gmail.com>
2024-09-25 19:03:20 +0900

    LeeSion 님이 변경한 내용

--- src/main/java/com/example/demo/controller/AnnonController.java
+++ src/main/java/com/example/demo/controller/AnnonController.java
@@ -1,13 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.BoardDTO;
 import com.example.demo.dto.PageRequestDTO;
-import com.example.demo.service.annonService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
